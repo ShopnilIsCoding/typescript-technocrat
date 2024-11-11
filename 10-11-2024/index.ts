ef83e2bd47c49c7fd23f71c0f1daa904f8ece58f
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-11-11 16:40:58 +0600

    type assertion,interface,generic

--- 10-11-2024/index.ts
+++ 10-11-2024/index.ts
@@ -32,3 +32,48 @@ catch(err)
 {
     console.log((err as CustomError).message);
 }
+
+interface User {
+    name: string;
+    age: number;
+}
+
+function greetUser(user: User): string {
+    return `Hello, ${user.name}! You are ${user.age} years old.`;
+}
+
+interface User2 extends User
+{
+    hobbies: string[];
+}
+
+const user2: User2 = {
+    name: "John Doe",
+    age: 30,
+    hobbies: ["reading", "painting"]
+};
+
+console.log(greetUser(user2)); 
+
+type Roll1=number[];
+interface Roll2{
+    [index : number]:number
+}
+
+const roll1:Roll1=[1,2,3,4,5];
+const roll2:Roll2=[1,2,3,4,5];
+
+interface Add2{
+    (num1:number,num2:number):number
+}
+
+const add:Add2=(num1,num2)=>num1+num2;
+
+type Generic<X,Y,Z>=[X,Y,Z?];
+const rollNumbers:Array<Generic<number,string,number>>=[[1,'rahim',166],[3,'karim'],[5,'halim']];
+console.log(rollNumbers);
+
+const userWithId:Generic<number,{name:string,email:string,role?:string},number>=[166,{
+    name:'John',
+    email:'john@example.com'
+},234897234]
